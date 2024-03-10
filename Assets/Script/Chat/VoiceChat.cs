3b403ae86294447f4fc436b6a1cb1c38deaaec18
anhndv <anhndv@rocketstudio.com.vn>
2024-03-10 17:02:18 +0700

    fix bug

---
+++ Assets/Script/Chat/VoiceChat.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.ComponentModel;
+using Unity.Services.Vivox;
+public class VoiceChat : MonoBehaviour
+{
+}
